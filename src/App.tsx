69306d6639a6465af1ec51115a772d138ad3092f
Kewyn1488 <kirtsikhanau@gmail.com>
2022-06-15 18:19:38 +0300

    header animation on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss'
 
 function App() {
 
-    const [active,setActive] = useState(false)
+    const [active, setActive] = useState(false)
 
-    const clickThis = (e:any) => {
+    const [scrolling, setScrolling] = useState(false);
+
+
+    useEffect(() => {
+
+        const header = document.querySelector('.header__container')
+        const headerContain = () => header?.classList.contains('round')
+
+        const onScroll = (e: any) => {
+            let currentPosition = window.scrollY;
+            const defaultOffset = 200
+
+            if (currentPosition > defaultOffset && !headerContain()) {
+                console.log(currentPosition)
+                console.log(headerContain())
+                header?.classList.add('round')
+                setScrolling(true)
+            } else if (currentPosition < defaultOffset && headerContain()) {
+                header?.classList.remove('round')
+                setScrolling(false)
+            }
+        }
+
+
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    }, [scrolling]);
+
+
+    const clickThis = (e: any) => {
         e.preventDefault()
         const iconMenu = document.querySelector(".burger")
-        if(iconMenu) {
+        if (iconMenu) {
             const menuHeader = document.querySelector(".header__menu")
             iconMenu.addEventListener("click", function (e) {
-                setActive((prev) => !prev)
+                setActive(!active)
                 iconMenu?.classList.toggle("_active")
                 menuHeader?.classList.toggle("_active")
             })
